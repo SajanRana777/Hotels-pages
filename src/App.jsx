a655392fc487612ed059a6c947f439b557d286c3
Sajan Rana <ranasajan509@gmail.com>
2025-08-12 23:12:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Navbar from "./Components/Navbar"
+import Hero from "./Components/hero"
+function App() {
+ return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+     )
+}
+
+export default App
