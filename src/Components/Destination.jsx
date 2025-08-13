314c87e67b0bf6ccc74493f2a9ede31242a03436
Sajan Rana <ranasajan509@gmail.com>
2025-08-13 20:06:41 +0530

    first

--- src/Components/Destination.jsx
+++ src/Components/Destination.jsx
@@ -10,7 +10,7 @@ const Destination = () => {
     <div className=" max-w-[1240px ]  mx-auto py-16 px-4 text-center">
       <h1> All-Inclusive Restors</h1>
       <p className="py-4"> On the Corlbbean's Best Beaches </p>
-      
+
       <div className=" grid grid-rows-none  md:grid-cols-5  py-4 gap-2  md:gap-4 ">
         <div className="relative">
           <img
@@ -18,7 +18,7 @@ const Destination = () => {
             src={Resorts1}
             alt="/"
           />
-          <div className= " bg-gray-900/30 absolute top-0 left-0 w-full h-full">
+          <div className=" bg-gray-900/30 absolute top-0 left-0 w-full h-full">
             <p className="left-3 bottom-3 text-2xl font-bold text-white absolute">
               {" "}
               Shimla
@@ -29,29 +29,38 @@ const Destination = () => {
           <img className="w-full h-full object-cover " src={Resorts2} alt="" />
           <div className="bg-gray-900/30 absolute top-0 left-0 w-full h-full">
             <p className="left-4 bottom-4 text-2xl font-bold text-white absolute">
-              {" "}
+              
               Kangra
             </p>
           </div>
         </div>
         <div className="relative">
-        <img className="w-full h-full object-cover " src={Pic} alt="/" />
-        <div className='bg-gray-900/30 absolute top-0 left-0 w-full h-full' >
-          <p className="left-3 bottom-3 text-2xl font-bold text-white absolute"> Manali</p>
-        </div>
+          <img className="w-full h-full object-cover " src={Pic} alt="/" />
+          <div className="bg-gray-900/30 absolute top-0 left-0 w-full h-full">
+            <p className="left-3 bottom-3 text-2xl font-bold text-white absolute">
+              
+              Manali
+            </p>
+          </div>
         </div>
         <div className="relative">
-        <img className="w-full h-full object- cover" src={Pic2} alt="/" />
-        <div className='bg-gray-900/30 absolute top-0 left-0 w-full h-full' >
-          <p className="left-3 bottom-3 text-2xl font-bold text-white absolute"> Dharamshala</p>
-        </div>
+          <img className="w-full h-full object- cover" src={Pic2} alt="/" />
+          <div className="bg-gray-900/30 absolute top-0 left-0 w-full h-full">
+            <p className="left-3 bottom-3 text-2xl font-bold text-white absolute">
+              
+              Dharamshala
+            </p>
+          </div>
         </div>
 
         <div className="relative">
-        <img className="w-full h-full object- cover" src={Pic3} alt="/" />
-        <div className='bg-gray-900/30 absolute top-0 left-0 w-full h-full' >
-          <p className="left-3 bottom-3 text-2xl font-bold text-white absolute"> Bir Billing</p>
-        </div>
+          <img className="w-full h-full object- cover" src={Pic3} alt="/" />
+          <div className="bg-gray-900/30 absolute top-0 left-0 w-full h-full">
+            <p className="left-3 bottom-3 text-2xl font-bold text-white absolute">
+              
+              Bir Billing
+            </p>
+          </div>
         </div>
       </div>
     </div>
